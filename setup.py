786d5abe73705dc3e540e7b6e127d71ab25bef14
Jimmy F <jimmy@thehumangeo.com>
2015-04-22 19:36:43 -0400

    Initial cleanup

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
+# -*- coding: utf-8 -*-
+
 try:
-	from setuptools import setup, Extension
+    from setuptools import setup, Extension
 except ImportError:
-	from distutils.core import setup, Extension
+    from distutils.core import setup, Extension
 
 # requiring C++ here for Windows support.
-c1=Extension('_geohash',
-	sources=['src/geohash.cpp',],
-	define_macros = [('PYTHON_MODULE',1),])
+c1 = Extension('_geohash',
+               sources=['src/geohash.cpp'],
+               define_macros=[('PYTHON_MODULE', 1)])
 
 setup(name='python-geohash',
-	version='0.8.5',
-	description='Fast, accurate python geohashing library',
-	author='Hiroaki Kawai',
-	url='http://code.google.com/p/python-geohash/',
-	py_modules=['geohash','quadtree','jpgrid','jpiarea'],
-	ext_modules = [c1]
-)
+      version='0.8.5',
+      description='Fast, accurate python geohashing library',
+      author='Hiroaki Kawai',
+      url='https://github.com/hkwi/python-geohash',
+      py_modules=['geohash', 'quadtree', 'jpgrid', 'jpiarea'],
+      ext_modules=[c1])
